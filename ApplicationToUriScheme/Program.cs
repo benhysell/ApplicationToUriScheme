be1154fe401948c7b78ae2f5466163e60029ccb8
ben hysell <ben.hysell@gmail.com>
2017-02-08 08:19:21 -0500

    initial add

---
+++ ApplicationToUriScheme/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationToUriScheme
+{
+    public class Program
+    {        
+        public static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+        public static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+
+            Console.WriteLine("\n\nArguments:\n");
+            foreach (string s in args)
+            {
+                Console.WriteLine("\t" + ProcessInput(s));
+            }
+
+            
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        static string ProcessInput(string s)
+        {
+            // TODO Verify and validate the input 
+            // string as appropriate for your application.
+            return s;
+        }
+    }
+}
